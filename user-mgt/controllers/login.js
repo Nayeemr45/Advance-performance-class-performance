d6c63dc141470275bbc492a5e5980ffc6d353aef
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2020-11-03 12:52:31 +0600

    Initial commit

---
+++ user-mgt/controllers/login.js
@@ -0,0 +1,25 @@
+const express 	= require('express');
+const router 	= express.Router();
+
+router.get('/', (req, res)=>{
+	res.render('login/index');
+});
+
+router.post('/', (req, res)=>{
+
+	//console.log(req.body.username);
+	//console.log(req.body.password);
+
+	if(req.body.username == req.body.password){
+		req.session.uname = req.body.username;
+		res.cookie('uname', req.body.username);
+		//res.cookie('pass', req.body.password);
+		res.redirect('/home');
+
+	}else{
+		res.redirect('/login');
+	}
+}); 
+
+
+module.exports = router;
